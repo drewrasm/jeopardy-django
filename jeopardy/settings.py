987b6812d18942f3ef17b12e6f6aa4afc6875320
Andrew Rasmussen <drewrasm@gmail.com>
2022-09-22 21:59:29 -0600

    some basic stuff

--- jeopardy/settings.py
+++ jeopardy/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uy9x%l9@kq97y1syl$ri6h&e-tx!(*&-kvse4kd^&b&s6qp#q_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SLACK_CLIENT_ID = os.environ.get('SLACK_CLIENT_ID', None)
+SLACK_CLIENT_SECRET = os.environ.get('SLACK_CLIENT_SECRET', None)
+SLACK_VERIFICATION_TOKEN = os.environ.get('SLACK_VERIFICATION_TOKEN', None)
+SLACK_BOT_USER_TOKEN = os.environ.get('SLACK_BOT_USER_TOKEN', None)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.conf import settings
+
+SLACK_VERIFICATION_TOKEN = getattr(settings, 'SLACK_VERIFICATION_TOKEN', None)
+
+class Events(APIView):
+    def post(self, request, *args, **kwargs):
+
+        slack_message = request.data
+
+        if slack_message.get('token') != SLACK_VERIFICATION_TOKEN:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        # verification challenge
+        if slack_message.get('type') == 'url_verification':
+            return Response(data=slack_message, status=status.HTTP_200_OK)   
+
+        return Response(status=status.HTTP_200_OK)
